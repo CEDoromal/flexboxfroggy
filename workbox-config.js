837377f4fe5f203ef1fe44162d6a59ccb91a40c4
CEDoromal <30488557+CEDoromal@users.noreply.github.com>
2022-04-29 13:36:04 +0800

    revised service worker

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: './',
+	globPatterns: [
+		'**/*.{yml,css,ico,png,jpg,svg,webp,html,js,json}'
+	],
+	swDest: 'sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
